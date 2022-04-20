a9f6b1a2203d9b387e0377e5c0f4752301407e41
Gilles Myny <91336156+GillesMyny1@users.noreply.github.com>
2022-04-19 20:52:20 -0400

    Update Piece.java

--- src/Piece.java
+++ src/Piece.java
@@ -1,13 +1,5 @@
 import java.util.*;
 
-/**
- * The Piece Class
- *
- * @author Jeremy Trendoff - 101160306
- * @version 3.0
- * @since Nov 22nd, 2021
- */
-
 public class Piece implements java.io.Serializable {
 
     /**
