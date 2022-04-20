8da2b602db145696c5a39ed2624fa776eb1b9e41
Gilles Myny <91336156+GillesMyny1@users.noreply.github.com>
2022-04-19 20:53:03 -0400

    Update SendJail.java

--- src/SendJail.java
+++ src/SendJail.java
@@ -1,11 +1,3 @@
-/**
- * The SendJail Class
- *
- * @author Gilles Myny
- * @version 3.0
- * @since Nov 22nd, 2021
- */
-
 public class SendJail extends Tile{
     /**
      * Constructor that calls super Tile class.
