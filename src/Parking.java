e9157dc17098ac807ef7cc08f17e6fff825d1496
Gilles Myny <91336156+GillesMyny1@users.noreply.github.com>
2022-04-19 20:52:10 -0400

    Update Parking.java

--- src/Parking.java
+++ src/Parking.java
@@ -1,11 +1,3 @@
-/**
- * The Parking class.
- *
- * @author Gilles Myny
- * @version 3.0
- * @since Nov 22nd, 2020
- */
-
 public class Parking extends Tile{
     /**
      * Constructor that calls super Tile class.
